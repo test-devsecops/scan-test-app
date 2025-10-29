f19b0daa96c1458276baa61de938898222d4bcbd
nantaswd <anantawijaya.swd@gmail.com>
2025-10-29 10:45:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ import urllib.request
 context = ssl._create_unverified_context()
 urllib.request.urlopen("https://example.com", context=context)
 
+# test
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
